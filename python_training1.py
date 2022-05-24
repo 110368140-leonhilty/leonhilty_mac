61e273cde409d657aab73765a8fc79d5fecc6e01
110368140-leonhilty <leon0970865551@gmail.com>
2022-05-24 16:32:43 +0800

    ok

--- python_training1.py
+++ python_training1.py
@@ -10,17 +10,31 @@ import random
 # print(random.sample(['22', '33', '44', '55', '66', '77', '88'], 4)) # ['44', '33', '22', '66']
 #-------------------------------
 
-str_dic = {
-    'zaa' : 101,
-    'hello': 97,
-    'good' : 98
-}
+# str_dic = {
+#     'zaa' : 101,
+#     'hello': 97,
+#     'good' : 98
+# }
+
+# a = []
+# sort_list=sorted(str_dic.items(), key=lambda x:x[1],reverse=True)
 
 
-a = []
-sort_list=sorted(str_dic.items(), key=lambda x:x[1],reverse=True)
+# for i in range(len(sort_list)):
+#     print(sort_list[i][0])
+
+'''
+string = ['hello 113','fff 97','abc 100']
+
+word_dic ={}
+
+for i in string:
+    keys = i.split(' ')[0]
+    values = i.split(' ')[1]
+    word_dic[keys] = values
 
 
-for i in range(len(sort_list)):
-    print(sort_list[i][0])
+print(word_dic.items())
+'''
+
 
